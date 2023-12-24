65df53ccd6dacfd3b4131b558d7dbb8a9bb47090
TorYev <zh.tor17@gmail.com>
2023-12-24 21:30:40 +0100

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,21 @@
-import user from '../data/user.json'
-import data from '../data/data.json'
+import user from '../data/user.json';
+import data from '../data/data.json';
 import { Profile } from './Profile/Profile';
-import {Statistics} from './Statistics/Statistics'
+import { Statistics } from './Statistics/Statistics';
 import { Fragment } from 'react';
 
 export const App = () => {
-  return (<Fragment>
-    <Profile
-   username={user.username}
-   tag={user.tag}
-   location={user.location}
-   avatar={user.avatar}
-   stats={user.stats}
-/>
-<Statistics title={title} stats={stats} />
-</Fragment>
+  return (
+    <Fragment>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+    </Fragment>
   );
 };
-
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,24 +1,15 @@
-export const Statistics = ({title, stats}) => {
-    return (<section class="statistics">
-    <h2 class="title">Upload stats</h2>
-    <ul class="stat-list">
-      <li class="item">
-        <span class="label">.docx</span>
-        <span class="percentage">4%</span>
-      </li>
-      <li class="item">
-        <span class="label">.mp3</span>
-        <span class="percentage">14%</span>
-      </li>
-      <li class="item">
-        <span class="label">.pdf</span>
-        <span class="percentage">41%</span>
-      </li>
-      <li class="item">
-        <span class="label">.mp4</span>
-        <span class="percentage">12%</span>
-      </li>
-    </ul>
-  </section>
-    )
-}
+import { StatisticItem } from './StatisticItem';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <>
+      {title && <h2 className="title">{title}</h2>}
+
+      <ul className="stat-list">
+        {stats.map(({ id, label, percentage }) => (
+          <StatisticItem key={id} label={label} percentage={percentage} />
+        ))}
+      </ul>
+    </>
+  );
+};
